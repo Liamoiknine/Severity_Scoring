b4253a36edfb6b64b3435cf67ccd4d638789fc31
Liamoiknine <liamoiknine1@gmail.com>
2025-06-05 16:09:22 +0300

    update for production

--- server/config.py
+++ server/config.py
@@ -1,6 +1,22 @@
 # config.py
+"""
 import os
 
 class Config:
     FIREBASE_CRED = os.getenv('FIREBASE_CREDENTIALS', '../../firebase.json')
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
+"""
+# config.py
+import os
+import json
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
+    
+    firebase_json = os.getenv('FIREBASE_CONFIG')
+    if firebase_json:
+        FIREBASE_CRED = json.loads(firebase_json)
+    else:
+        # Fallback to file path for local dev
+        with open('../../firebase.json') as f:
+            FIREBASE_CRED = json.load(f)
